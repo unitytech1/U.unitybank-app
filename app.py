735838ccdf8c1f7260cac7e25497b50bea51e3e9
unity3478876 <obiomajessica6@gmail.com>
2026-03-30 17:08:28 +0100

    fixed email config issue

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USERNAME'] = 'j99310482@gmail.com'
+app.config['MAIL_USERNAME'] = 
 os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = 'pfblqitewcbapgiz'
+app.config['MAIL_PASSWORD'] = 
 os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = ('UnityBank', 'j99310482@gmail.com')  
 
