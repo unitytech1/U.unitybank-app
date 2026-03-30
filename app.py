5181d06079dedd1712004cd0f63de8f099d40f58
unity3478876 <obiomajessica6@gmail.com>
2026-03-30 16:32:45 +0100

    Fixed OTP redirect issue

--- app.py
+++ app.py
@@ -34,9 +34,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = 'j99310482@gmail.com'
-os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = 'pfblqitewcbapgiz'
-os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = ('UnityBank', 'j99310482@gmail.com')  
 
 # INIT
