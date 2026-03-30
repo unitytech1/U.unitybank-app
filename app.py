a38658aa0405cc7efba85150db9c95956eab25b8
unity3478876 <obiomajessica6@gmail.com>
2026-03-30 17:51:07 +0100

    fixed enviro

--- app.py
+++ app.py
@@ -33,10 +33,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USERNAME'] = 'j99310482@gmail.com'
-os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = 'pfblqitewcbapgiz'
-os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = ('UnityBank', 'j99310482@gmail.com')  
 
 # INIT
